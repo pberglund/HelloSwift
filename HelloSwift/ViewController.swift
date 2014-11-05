03e424a3b47a2a5fb769b85e0fba15f7ccef93a5
pberglund <pberglund@sagepath.com>
2014-11-05 10:00:59 -0500

    base hello world app project
    working hello world in Xcode 6

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupUi()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sayHelloAction(sender: AnyObject) {
+        let name = nameTextField.text
+        
+        if name.isEmpty {
+            
+            let alert = UIAlertController(title: "Error", message: "Please enter a name",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,
+                handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            helloLabel.text = "Hello \(name)!"
+        }
+    }
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var sayHelloButton: UIButton!
+    
+    func setupUi(){
+        helloLabel.text = "Hello Swift!"
+        helloLabel.textColor = UIColor.redColor()
+        helloLabel.textAlignment = NSTextAlignment.Center
+        nameTextField.placeholder = "Enter your name"
+        sayHelloButton.setTitle("Say Hello", forState: .Normal)
 
+    }
 }
 
